c1067f875156b861de4d132cd9abdfb5f5a6a9be
atomhand <thessalikon@live.co.uk>
2024-08-28 10:20:45 +0100

    window name + disabled vsync

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use bevy::prelude::*;
 use bevy_mod_picking;
-
+use bevy::window::{PresentMode, WindowTheme};
 mod camera;
 mod galaxy;
 mod graphics;
@@ -19,7 +19,19 @@ fn main() {
 
     App::new()
         .add_plugins((
-            DefaultPlugins,
+            DefaultPlugins.set(WindowPlugin{
+                primary_window: Some(Window {
+                    title: "Cynewulf".into(),
+                    name: Some("bevy.app".into()),
+                    //resolution: (1920.,1080.).into(),
+                    present_mode: PresentMode::AutoNoVsync,
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    window_theme: Some(WindowTheme::Dark),
+                    ..default()
+                }),
+                ..default()
+            }),
             galaxy::GalaxySetupPlugin,
             graphics::GraphicsPlugin,
             simulation::SimulationPlugin,
