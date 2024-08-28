93edb0b9c8eeb9acca9d02f8e15013e8aba44422
atomhand <thessalikon@live.co.uk>
2024-08-28 22:54:48 +0100

    several camera improvements

--- src/camera.rs
+++ src/camera.rs
@@ -2,6 +2,7 @@ use bevy::{input::mouse::MouseWheel, prelude::*};
 
 use crate::galaxy::GalaxyConfig;
 use super::galaxy::Selection;
+use crate::prelude::*;
 
 pub struct CameraPlugin;
 
@@ -39,6 +40,7 @@ pub struct CameraMain {
     target_pos: Vec3,
     star_pos : Vec3,
     star_local_pos: Vec3,
+    system_radius : f32,
     zoom: f32,
     pub mode_transition: f32
 }
@@ -48,6 +50,7 @@ impl Default for CameraMain {
         Self {
             target_pos: Vec3::new(0.0, 0., 0.0),
             zoom: 1.0,
+            system_radius : 1.0,
             star_local_pos : Vec3::ZERO,
             star_pos : Vec3::ZERO,
             mode_transition: 0.0,
@@ -58,6 +61,28 @@ impl Default for CameraMain {
 impl CameraMain {
     pub fn adjusted_mode_transition(&self) -> f32 {
         smootherstep(0.0,1.0,self.mode_transition)
+    }
+
+    fn translation(&self, transition : f32, galaxy_scale : f32) -> Vec3 {
+        let adjusted_system_scale = self.system_radius * 2.0;
+
+        let star_mode_look_pos = self.star_pos + self.star_local_pos + Vec3::new(0.0,0.0,0.0);
+        let look_pos = self.target_pos * (1.0 - transition) + transition * star_mode_look_pos;
+        
+        let galaxy_zoom =self.zoom * 0.85 + 0.15;
+        let adjusted_galaxy_scale = galaxy_scale * galaxy_zoom;
+
+        let adjusted_scale = adjusted_galaxy_scale * (1.0 - transition) + adjusted_system_scale * transition;
+
+        let antitilt = 0.6;
+        look_pos + Vec3::new(0., adjusted_scale, -adjusted_scale * antitilt)
+    }
+
+    fn look_pos(&self, transition : f32) -> Vec3 {
+        let system_scale = self.system_radius * 2.0;
+        let star_mode_look_pos = self.star_pos + self.star_local_pos
+            + Vec3::new(0.0,0.0,-0.1 * system_scale);
+        self.target_pos * (1.0 - transition) + transition * star_mode_look_pos
     }
 }
 
@@ -100,7 +125,7 @@ fn smootherstep(edge0 : f32, edge1 : f32, x : f32) -> f32 {
 
 pub fn camera_control_system(
     mut query: Query<(&mut Transform, &mut CameraMain)>,
-    transform_query: Query<&Transform, Without<CameraMain>>,
+    star_query: Query<&Star, Without<CameraMain>>,
     mut camera_settings : ResMut<CameraSettings>,
     keys: Res<ButtonInput<KeyCode>>,
     time: Res<Time>,
@@ -109,18 +134,8 @@ pub fn camera_control_system(
     mut scroll_evr: EventReader<MouseWheel>,
 ) {
     let galaxy_scale = galaxy_config.radius * 2.5;
-
     let (mut transform, mut camera) = query.get_single_mut().expect("Error: Require ONE camera");
 
-    // TODO - BLENDED TRANSITIONS BETWEEN CAMERA MODES
-    // ie. have an anim s value
-    // Calculate positions for "galaxy camera" and "star camera"
-    // Lerp
-    //
-    // Input/camera movement only calculated for active mode
-    //
-    // In the future, need a system for smoothly deciding when to start drawing planets etc.
-    // Efficient 
     match camera_settings.camera_mode {
         CameraMode::Star => {            
             let mut wheel_ev : f32 = 0.0;
@@ -137,13 +152,14 @@ pub fn camera_control_system(
             }
         },
         CameraMode::Galaxy => {
-            if let Some(star) = selection.zoomed_system {
+            if let Some(star_ent) = selection.zoomed_system {
                 camera_settings.camera_mode = CameraMode::Star;
                 camera_settings.visibility_updated = false;
-                //controller.zoom = 0.0;
                 camera.star_local_pos = Vec3::ZERO;
-                camera.star_pos = transform_query.get(star).unwrap().translation;
-                camera_settings.star = Some(star);
+                let star = star_query.get(star_ent).unwrap();
+                camera.star_pos = star.pos;
+                camera.system_radius = star.system_radius_actual();
+                camera_settings.star = Some(star_ent);
             }
         }
     }
@@ -163,22 +179,18 @@ pub fn camera_control_system(
         key_delta.x -= 1.0;
     }
 
-    // 
-
     let transition_speed = 4.0 * time.delta_seconds();
 
     // Update 
     match camera_settings.camera_mode {
         CameraMode::Star => {
             let speed: f32 = GalaxyConfig::AU_SCALE * 6.0 * time.delta_seconds();
-     
-            // TODO: CLAMP TO SYSTEM DIAMETER
             camera.star_local_pos += key_delta * speed;
 
-            // Adjust mode transition
+            if camera.star_local_pos.length() > camera.system_radius {
+                camera.star_local_pos = camera.star_local_pos.normalize() * camera.system_radius;
+            }
             camera.mode_transition = (camera.mode_transition  + transition_speed).min(1.0);
-
-            // star selection gfraphic
         },
         CameraMode::Galaxy => {
             for ev in scroll_evr.read() {
@@ -191,46 +203,18 @@ pub fn camera_control_system(
                     }
                 }
             }        
-        
             camera.zoom = camera.zoom.clamp(0., 1.);
-            let tzoom = camera.zoom * 0.85 + 0.15;
-        
+            let tzoom = camera.zoom * 0.85 + 0.15;        
             let speed: f32 = (tzoom* galaxy_scale) * 0.5 * time.delta_seconds();
-        
-            let world_bounds_max = Vec2::splat(galaxy_config.radius);   
-
-            let world_bounds_min = -world_bounds_max;   
             camera.target_pos += key_delta * speed;
-            camera.target_pos.x = camera.target_pos.x.clamp(world_bounds_min.x, world_bounds_max.x);
-            camera.target_pos.z = camera.target_pos.z.clamp(world_bounds_min.y, world_bounds_max.y);
-
-            /*  
-            transform.translation = controller.target_pos + Vec3::new(0., scale,-scale * (1.0-tzoom * 0.75)) * tzoom;
-            transform.look_at(controller.target_pos, Vec3::Y);
-            */
-            // Adjust mode transition
-
+            let d = camera.target_pos.xz().length();
+            if d > galaxy_config.radius {
+                camera.target_pos *= galaxy_config.radius / d;
+            }
             camera.mode_transition = (camera.mode_transition - transition_speed).max(0.0);
-
         }
     }
-    
-    // Apply blended camera position + rotation with mode transition
 
-    let adjusted_mode_transition = camera.adjusted_mode_transition();
-
-    let star_mode_look_pos = camera.star_pos + camera.star_local_pos;
-    let look_pos = camera.target_pos * (1.0 - adjusted_mode_transition) + adjusted_mode_transition * star_mode_look_pos;
-    
-    let galaxy_zoom =camera.zoom * 0.85 + 0.15;
-    let adjusted_galaxy_scale = galaxy_scale * galaxy_zoom;
-
-    let adjusted_system_scale = GalaxyConfig::AU_SCALE * 16.0;
-
-    let adjusted_scale = adjusted_galaxy_scale * (1.0 - adjusted_mode_transition) + adjusted_system_scale * adjusted_mode_transition;
-
-    //transform.translation = look_pos + Vec3::new(0., galaxy_scale,-galaxy_scale * (1.0-azoom * 0.75)) * azoom;
-    let antitilt = 0.6;
-    transform.translation = look_pos + Vec3::new(0., adjusted_scale, -adjusted_scale * antitilt);
-    transform.look_at(look_pos, Vec3::Y);
+    transform.translation = camera.translation(camera.adjusted_mode_transition(), galaxy_scale);
+    transform.look_at(camera.look_pos(camera.adjusted_mode_transition()), Vec3::Y);
 }
