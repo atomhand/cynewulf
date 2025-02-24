947c0ef1d03470a29d6968a79fdacc995101d3e5
atomhand <thessalikon@live.co.uk>
2025-02-24 00:29:02 +0000

    added some comments

--- src/galaxy/galaxy_config.rs
+++ src/galaxy/galaxy_config.rs
@@ -6,12 +6,18 @@ pub struct GalaxyConfig {
     pub max_stars: i32,
     pub spacing: f32,
 }
+
+// NOTE
+// The world space coordinate system is scaled to parsecs, for now
+// In the future it would probably be wise to use separate coordinate systems for the galaxy and systems
+//  --- The transitions will get a bit fiddly tho which is why I have not bothered for now
+//  --- I'm assuming precision errors will eventually become a problem and force my hand
+
 impl GalaxyConfig {
-    pub const AU_SCALE: f32 = 0.1;
+    pub const AU_SCALE: f32 = 0.1; // scale of 1 AU to a Parsec
     pub const CELESTIAL_BODIES_SCALE: f32 = 20.0; // boost to the radius of celestial bodies relative to distance
     pub const PLANETS_SCALE: f32 = 1.0; // radius of a jupiter-sized planet relative to a sun-size star
-    pub const SOLAR_RADIUS: f32 = 0.00465 * Self::AU_SCALE * Self::CELESTIAL_BODIES_SCALE; // Radius of Sol
-                                                                                           // === 0.0093
+    pub const SOLAR_RADIUS: f32 = 0.00465 * Self::AU_SCALE * Self::CELESTIAL_BODIES_SCALE; // Radius of Sol in Au
     pub const MAX_SYSTEM_BODIES: usize = 12; // Used for UI slots and stuff
 
     pub const HYPERLANE_VISUAL_STAR_CLEARANCE: f32 = 10.0;
@@ -21,12 +27,9 @@ impl GalaxyConfig {
 impl Default for GalaxyConfig {
     fn default() -> Self {
         Self {
-            radius: 500.0,
+            radius: 500.0, // in parsecs
             max_stars: 1000,
             spacing: 40.0,
-            // Scale of 1 AU to a parsec
-            // This doesn't need to be scaled realistically, it's just chosen to create a pleasant zoom transition from Galaxy-System view,
-            // and to allow the use of a shared coordinate system for rendering
         }
     }
 }
